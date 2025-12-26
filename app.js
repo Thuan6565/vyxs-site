d15ba57fe2b145c33f536dae3275007de87166fe
Thuan6565 <rdgedg294@gmail.com>
2025-12-26 16:03:24 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -29,3 +29,28 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     }
 });
+
+fetch("scripts.json")
+  .then(res => res.json())
+  .then(data => {
+    const container = document.getElementById("scripts-container");
+
+    data.forEach(script => {
+      const box = document.createElement("div");
+      box.className = "script-box";
+
+      box.innerHTML = `
+        <h2>${script.title}</h2>
+        ${script.description ? `<p>${script.description}</p>` : ""}
+        <button onclick="openGit('${script.link}')">
+            Get on GitHub
+        </button>
+      `;
+
+      container.appendChild(box);
+    });
+  });
+
+function openGit(url) {
+    window.open(url, "_blank");
+}
