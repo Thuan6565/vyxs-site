f2b521bb7d5a0dc9a6a1243e97af199cc76276e0
Thuan6565 <rdgedg294@gmail.com>
2025-12-26 14:22:56 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+// Button Scripts -> scripts.html
+document.addEventListener("DOMContentLoaded", () => {
+    const scriptsBtn = document.getElementById("scriptsBtn");
+
+    if (scriptsBtn) {
+        scriptsBtn.addEventListener("click", () => {
+            window.location.href = "scripts.html";
+        });
+    }
+});
+
+// Button Scripts -> scripts.html
+document.addEventListener("DOMContentLoaded", () => {
+    const scriptsBtn = document.getElementById("getbtn");
+
+    if (scriptsBtn) {
+        scriptsBtn.addEventListener("click", () => {
+            window.location.href = "scripts.html";
+        });
+    }
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+    const scriptsBtn = document.getElementById("upbtn");
+
+    if (scriptsBtn) {
+        scriptsBtn.addEventListener("click", () => {
+            window.location.href = "upload.html";
+        });
+    }
+});
